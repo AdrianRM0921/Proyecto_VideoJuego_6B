f9a92d309edd8ea90c406d1ae9cf126873e3b569
AdrianRM0921 <adrimon0921@gmail.com>
2024-07-04 19:44:29 -0600

    Update enemigoS.js

--- scenes/modo_historia/players/enemigoS.js
+++ scenes/modo_historia/players/enemigoS.js
@@ -1,9 +1,9 @@
 export class EnemigoS {
     constructor(scene, player) {
         this.scene = scene;
-        this.player = player; // Referencia al jugador para detectar su proximidad
-        this.isAttacking = false; // Para evitar múltiples ataques simultáneos
-        this.enemigos = []; // Array para almacenar los enemigos creados
+        this.player = player; 
+        this.isAttacking = false; 
+        this.enemigos = []; 
     }
 
     preload() {
@@ -22,17 +22,28 @@ export class EnemigoS {
         this.scene.load.image('muerte2_D', 'assets/img/FIFAS/enemigo1/muerted2.png');
         this.scene.load.image('muerte1_I', 'assets/img/FIFAS/enemigo1/muertei1.png');
         this.scene.load.image('muerte2_I', 'assets/img/FIFAS/enemigo1/muertei2.png');
+
+        // Cargar imágenes de vida del enemigo
+        this.scene.load.image('vida_enemigo1', 'assets/img/FIFAS/VIDA/VIDA100.png');
+        this.scene.load.image('vida_enemigo2', 'assets/img/FIFAS/VIDA/VIDA75.png');
+        this.scene.load.image('vida_enemigo3', 'assets/img/FIFAS/VIDA/VIDA50.png');
+        this.scene.load.image('vida_enemigo4', 'assets/img/FIFAS/VIDA/VIDA25.png');
+        this.scene.load.image('vida_enemigo5', 'assets/img/FIFAS/VIDA/VIDA1.png');
+        this.scene.load.image('vida_enemigo6', 'assets/img/FIFAS/VIDA/VIDA0.png');
     }
 
     create(x, y) {
         this.enemy = this.scene.physics.add.sprite(x, y, 'parado1');
-        // Configuración física, tamaño, colisiones, etc.
         this.enemy.body.setSize(this.enemy.width * 0.4, this.enemy.height * 0.6);
         this.enemy.body.setOffset(this.enemy.width * 0.3, this.enemy.height * 0.1);
         this.enemy.setBounce(0.2);
         this.enemy.setScale(2);
         this.enemy.setCollideWorldBounds(true);
         
+        this.enemy.vida = 50; // Asigna vida al enemigo
+        this.enemy.maxVida = 50; // Asigna vida máxima al enemigo
+        this.enemy.barraVida = this.scene.add.image(x, y - 50, 'vida_enemigo1').setDepth(1);
+        this.enemy.barraVida.setScale(0.5);
 
         // Crear animaciones
         this.scene.anims.create({
@@ -72,35 +83,60 @@ export class EnemigoS {
 
     update() {
         // Lógica de movimiento, detección de jugador, ataque, etc.
-        // Ejemplo básico de seguimiento de jugador
         const distancia = Phaser.Math.Distance.Between(this.enemy.x, this.enemy.y, this.player.x, this.player.y);
 
         if (distancia < 300) {
-            // Si el jugador está lo suficientemente cerca, realizar alguna acción (ataque, movimiento, etc.)
             if (!this.isAttacking) {
                 this.isAttacking = true;
-                // Lógica de ataque o movimiento hacia el jugador
-             
-                this.scene.physics.moveToObject(this.enemy, this.player, 100);
-                // Determinar dirección del jugador para animación de ataque
+                this.scene.physics.moveToObject(this.enemy, this.player, 100)
                 if (this.enemy.x < this.player.x) {
-                    this.enemy.anims.play('atacar_D', true); // Activar animación de ataque hacia la derecha
+                    this.enemy.anims.play('atacar_D', true);
                 } else {
-                    this.enemy.anims.play('atacar_I', true); // Activar animación de ataque hacia la izquierda
+                    this.enemy.anims.play('atacar_I', true);
                 }
             }
         } else {
-            // Si el jugador está lejos, detener cualquier acción de ataque o seguimiento
             this.isAttacking = false;
             this.enemy.body.setVelocity(0);
-            // Determinar dirección del jugador para animación de caminar
+            
             if (this.enemy.x < this.player.x) {
-                this.enemy.anims.play('caminar_D', true); // Activar animación de caminar hacia la derecha
+                this.enemy.anims.play('caminar_D', true); 
             } else {
-                this.enemy.anims.play('caminar_I', true); // Activar animación de caminar hacia la izquierda
+                this.enemy.anims.play('caminar_I', true); 
             }
         }
 
+        // Actualizar la barra de vida del enemigo
+        this.enemy.barraVida.setPosition(this.enemy.x, this.enemy.y - 50);
+    }
+
+    receiveDamage(enemy, amount) {
+        if (enemy.vida <= 0) return;
         
+        enemy.vida -= amount;
+        if (enemy.vida < 0) {
+            enemy.vida = 0;
+        }
+        
+        // Actualiza la textura de la barra de vida basada en la salud restante
+        const escala = enemy.vida / enemy.maxVida;
+        if (escala >= 0.75) {
+            enemy.barraVida.setTexture('vida_enemigo1');
+        } else if (escala >= 0.5) {
+            enemy.barraVida.setTexture('vida_enemigo2');
+        } else if (escala >= 0.25) {
+            enemy.barraVida.setTexture('vida_enemigo3');
+        } else if (escala > 0) {
+            enemy.barraVida.setTexture('vida_enemigo4');
+        } else {
+            enemy.barraVida.setTexture('vida_enemigo6');
+            // Reproduce animación de muerte y elimina enemigo
+            enemy.anims.play('muerte1_D', true);
+            this.scene.time.delayedCall(500, () => {
+                enemy.destroy();
+                enemy.barraVida.destroy();
+            });
+        }
     }
 }
+
